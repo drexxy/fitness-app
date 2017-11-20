9d7a0bbb661db53a9b53aee9951f1d4a3e4b6f21
Benjamin Hornsby <benhornsby_hornsby_2853@wegotcoders.com>
2017-11-20 10:41:26 +0000

    Required rack_session_access

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,7 @@
 # files.
 
 require 'cucumber/rails'
+require 'rack_session_access/capybara'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
