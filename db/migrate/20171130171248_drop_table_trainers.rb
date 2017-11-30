c2bc19f2ad41ced35b28ae34fbb06c717bb02d09
Benjamin Hornsby <benhornsby_hornsby_2853@wegotcoders.com>
2017-11-30 17:15:11 +0000

    Removed unused trainers table and model

---
+++ db/migrate/20171130171248_drop_table_trainers.rb
@@ -0,0 +1,5 @@
+class DropTableTrainers < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :trainers
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130100810) do
+ActiveRecord::Schema.define(version: 20171130171248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,11 +103,6 @@ ActiveRecord::Schema.define(version: 20171130100810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id"
-  end
-
-  create_table "trainers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "workouts", force: :cascade do |t|
